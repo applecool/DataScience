e6f1d04e93cf1818a8ec7ddc2afacb45b7b9e536
applecool <kmitharsha@gmail.com>
2015-08-31 12:04:44 -0500

    added names rbind.r

--- R/3.0/rbind.r
+++ R/3.0/rbind.r
@@ -5,3 +5,9 @@ return_jedi <- c(309.306, 165.8)
 
 # Create star_wars_matrix
 star_wars_matrix <- rbind(new_hope, empire_strikes, return_jedi)
+print(star_wars_matrix)
+
+# Name the columns and rows of star_wars_matrix
+colnames(star_wars_matrix) <- c("US","non-US")
+rownames(star_wars_matrix) <- c("A New Hope","The Empire Strikes Back","Return of the Jedi")
+print(star_wars_matrix)
