b5df75bc20333ea359b59da5194915118cadf361
applecool <kmitharsha@gmail.com>
2015-08-31 20:26:28 -0500

    updated shining_list.r

--- R/5.1/shining_list.r
+++ R/5.1/shining_list.r
@@ -7,3 +7,9 @@ reviews_factor <- factor(c("Good", "OK", "Good", "Perfect", "Bad", "Perfect", "G
 shining_list <- list(title="The Shining", actors=actors_vector, reviews=reviews_factor)
 print(shining_list)
 str(shining_list)
+
+# Add both the year and director to shining_list: shining_list_ext
+shining_list_ext <- c(shining_list, year=1980, director="Stanley Kubrick")
+
+# Have a look at the structure of shining_list_ext
+str(shining_list_ext)
