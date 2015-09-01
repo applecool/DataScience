cb6524192d1da8a7bf12e9197f8dfcef399d25db
applecool <kmitharsha@gmail.com>
2015-08-31 21:00:14 -0500

    added planets.r

--- R/6.1/dataframe.r
+++ R/6.1/dataframe.r
@@ -1,8 +1,12 @@
+#mtcars is a cars dataset
 # Print the first observations of mtcars
-
 head(mtcars)
-# Print the last observations of mtcars
 
+# Print the last observations of mtcars
 tail(mtcars)
+
 # Print the dimensions of mtcars
 dim(mtcars)
+
+# Investigate the structure of the mtcars data set
+str(mtcars)
