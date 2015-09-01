60e79856e4d72dcd3b06a60d3455eb8992c4a1cd
applecool <kmitharsha@gmail.com>
2015-09-01 09:49:20 -0500

    updated custom_plots.r

--- R/7.2/custom_labels.r
+++ R/7.2/custom_labels.r
@@ -1,4 +1,11 @@
 # movies is pre-loaded in your workspace
 
-# Create a customized plot
-plot(movies$votes,movies$runtime,main="Votes versus Runtime",xlab="Number of votes [-]" ,ylab= "Runtime [s]",sub= "No clear correlation")
+# Customize the plot further
+plot(movies$votes, movies$runtime,
+     main = "Votes versus Runtime",
+     xlab = "Number of votes [-]",
+     ylab = "Runtime [s]",
+     sub = "No clear correlation",
+     pch = 9,
+     col = "#dd2d2d",
+     col.main = 604)
