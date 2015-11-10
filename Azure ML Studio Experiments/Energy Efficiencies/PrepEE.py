e43902868c8bc0e068179b4ff19f78618c28fd51
applecool <kmitharsha@gmail.com>
2015-11-10 16:59:16 -0600

    added prepEE.py

---
+++ Azure ML Studio Experiments/Energy Efficiencies/PrepEE.py
@@ -0,0 +1,20 @@
+
+## Load the data
+import pandas as pd
+import os
+from sklearn import preprocessing
+
+pathName = "/Users/ShellZero/DataScience/"
+fileName = "EnergyEfficiencyRegressiondata.csv"
+filePath = os.path.join(pathName, fileName)
+eeframe = pd.read_csv(filePath)
+
+## Remove columns we're not going to use
+eeframe = eeframe.drop('Cooling Load', 1)
+
+## scale numeric features
+scaleList = ["Relative Compactness", "Surface Area", \
+       "Wall Area", "Roof Area", "Glazing Area",  \
+       "Glazing Area Distribution"]
+arry = eeframe[scaleList].as_matrix()
+eeframe[scaleList] = preprocessing.scale(arry)
